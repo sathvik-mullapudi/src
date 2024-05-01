582ea75c3c6544097e7dcdef1f74179c7c2c1ebe
sathvik-mullapudi <sathvik-mullapudi@users.noreply.github.com>
2024-05-01 12:18:23 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="sathvik-mullapudi"
+original_name="src"
+original_urlname="src"
+original_description="Awesome src created by sathvik-mullapudi"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv src $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
